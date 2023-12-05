cc7b59b495e92fa46befc116f65ccf2526e40e47
Diya Wadhwani <wadhwani2399@gmail.com>
2023-12-05 06:14:11 -0800

    Contact feature

--- finalproject/AboutScreen/AboutScreenViewController.swift
+++ finalproject/AboutScreen/AboutScreenViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class AboutScreenViewController: UIViewController {
     
@@ -14,10 +15,45 @@ class AboutScreenViewController: UIViewController {
     override func loadView() {
         view = aboutView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupRightNavBar()
+        
+    }
+    
+    func setupRightNavBar() {
+        
+        let emailIcon = UIImage(systemName: "envelope.fill")
+        let emailButton = UIBarButtonItem(image: emailIcon, style: .plain, target: self, action: #selector(emailButtonTapped))
+        
+        // Set a custom size for the bar button item
+        emailButton.image = emailIcon?.withRenderingMode(.alwaysOriginal).withTintColor(AppColors.greenButton)
+        emailButton.width = 40
+        
+        navigationItem.rightBarButtonItem = emailButton
+        
+    }
+    
+    @objc private func emailButtonTapped() {
+        // Open email option
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposeViewController = MFMailComposeViewController()
+            mailComposeViewController.mailComposeDelegate = self
+            mailComposeViewController.setToRecipients(["wadhwani.d@northeastern.edu"])
+            mailComposeViewController.setSubject("Feedback")
+            present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            // Handle the case where the device is not configured for email
+            print("Device not configured for email")
+        }
+    }
+}
+
+extension AboutScreenViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
 
